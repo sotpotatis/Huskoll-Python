f2aae8677f23eaf2ac40b81c299a21454c7bae9f
sotpotatis <72197132+sotpotatis@users.noreply.github.com>
2020-12-23 19:37:25 +0100

    Create Exceptions.py

---
+++ lib/huskoll/Exceptions.py
@@ -0,0 +1,6 @@
+"""Exceptions.py
+Define exceptions used by the library."""
+class ResponseError(Exception):
+    """Own class for exceptions related
+    to response errors. Currently only present in two places."""
+    pass #Don't do anything else than just define the function
